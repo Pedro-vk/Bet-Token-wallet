79892b94ead83e4e6fa580638f04424f39373b65
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-01-15 01:33:11 +0100

    Initial wallet and bets visualization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import blockies = require('blockies');
 
-import { BetTokenService } from './shared';
+import { BetTokenService, Token, Bet } from './shared';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private betTokenService: BetTokenService) { }
+export class AppComponent implements OnInit {
+  account: string;
+  token: Token;
+  balance$: Observable<number>;
+  debt$: Observable<number>;
+  myBets$: Observable<Bet[]>;
 
+  constructor(private betTokenService: BetTokenService, private domSanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.balance$ = this.betTokenService.getBalanceChanges();
+    this.debt$ = this.betTokenService.getDebtChanges();
+    this.myBets$ = this.betTokenService.getMyBetsChanges();
+
+    this.betTokenService
+      .getToken()
+      .subscribe(token => this.token = token);
+
+    this.betTokenService
+      .getAccont()
+      .subscribe(account => this.account = account);
+
+    console.log(this);
+  }
+
+  getImageOf(account: string = ''): SafeStyle {
+    return this.domSanitizer.bypassSecurityTrustStyle(
+      `url(${blockies({seed: account.toLowerCase(), size: 8, scale: 8}).toDataURL()})`,
+    );
+  }
+
+  trackBet(index: number, bet: Bet): string {
+    return String(bet.id) || '';
+  }
 }
